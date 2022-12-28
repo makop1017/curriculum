f039c3cf58ce93ace79f42c2b70ea55e18ae431e
makop1017 <114816765+makop1017@users.noreply.github.com>
2022-12-28 22:53:20 +0900

    課題提出

--- JavaStudy/2-4/src/fruits/Fruits.java
+++ JavaStudy/2-4/src/fruits/Fruits.java
@@ -4,6 +4,6 @@ public class Fruits {
 
     public static void printFruits(String fruits, int count) {
         System.out.println("食べ物は" + fruits);
-        	
+        System.out.println(count + "個です");    	
     }
 }
--- JavaStudy/2-4/src/study/Study.java
+++ JavaStudy/2-4/src/study/Study.java
@@ -22,37 +22,10 @@ public class Study {
          *     また、課題の画像と同じ表示になるように
          *     printFruitsの引数にはFruitsConstantsに指定されている定数を設定してください。
          */
-    	public class Fruits{
-    		public static void printFruits() {
-    			
-    			String FRUITS_APPLE_01 = "りんご";
-    		    String FRUITS_GRAPE_02 = "ぶどう";
-    		    String FRUITS_WATERMELON_03 = "スイカ";
-    		    String FRUITS_LEMON_04 = "レモン";
-    		    String FRUITS_MELON_05 = "メロン";
-    		    
-    		    String fruits = fruits.printFruits(FRUITS_APPLE_01,FRUITS_GRAPE_02,FRUITS_WATERMELON_03,FRUITS_LEMON_04);
-    		
-    		public class FruitsConstants{
-    			public static void FruitsConstants {
-    			
-    		    Integer FRUITS_COUNT_10 = 10;
-    		    Integer FRUITS_COUNT_20 = 20;
-    		    Integer FRUITS_COUNT_45 = 45;
-    		    Integer FRUITS_COUNT_78 = 78;
-    		    Integer FRUITS_COUNT_100 = 100; 
-    		    
-    		    Integer constants = constants.FruitsConstants()
-    			
-    		
-    		    System.out.println("食べ物は" + fruits);
-    		    System.out.println(count + "個です");
-    			}
-    		
-    		
-    		}
-    	}
-    	
+        Fruits fruits = new Fruits;
+        fruits.FruitsConstants(fruits,count);
 
+        System.out.println("食べ物は" + FRUITS_LEMON_04);
+        System.out.println(FRUITS_COUNT_20 + "個です");
     }
 }

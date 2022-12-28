67938defeef9652404567c53785f75aa6dc10fbd
makop1017 <114816765+makop1017@users.noreply.github.com>
2022-12-28 21:55:50 +0900

    課題提出

--- JavaStudy/2-3/src/Task.java
+++ JavaStudy/2-3/src/Task.java
@@ -1,11 +1,17 @@
 package study;
 
 // ① TaskクラスにCalculatorクラスを継承させなさい。
-public class Calculator{
-	protected int a;
-	protected int b;
-	protected int c;
-	
+public class Task{
+
+    protected Calculator(){
+    
+    }
+    protected Calculator(int a,int b){
+
+    }
+    protected Calcuiator(int a,int b,int c){
+
+    }
 }
 
     /**
@@ -15,13 +21,7 @@ public class Calculator{
 
         // ② Calculator.javaのすべてのオーバーロードメソッド「plus」に適当な引数を与え、下記画像のよう出力されるようコーディングしなさい。
         // 尚、「どのクラスから呼び出しているか」を明確にするため、plus()には呼び出し元のキーワードを付与すること。
-    	public Calculator(int a, int b) {
-    		
-    	}
-    	public Calctor(int a, int b, int c) {
-    		
-    	}
-    	
-    	}
-
+    	System.out.println("plusメソッドの引数が1つの場合:" + int a);
+        System.out.println("plusメソッドの引数が2つの場合:" + int b);
+        System.out.println("plusメソッドの引数が3つの場合:" + int c);
     }
